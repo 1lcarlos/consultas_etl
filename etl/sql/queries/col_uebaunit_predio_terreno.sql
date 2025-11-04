6c421a4538c2ff912dc27f792dcdce6f0560f011
3erMundo <jacobogopa@gmail.com>
2025-11-04 15:24:47 -0500

    add query predio terreno , unidad de construccion

---
+++ etl/sql/queries/col_uebaunit_predio_terreno.sql
@@ -0,0 +1,6 @@
+SELECT  
+t.id as ue_gc_terreno,
+unidad
+
+FROM {schema}.col_uebaunit cu
+JOIN {schema}.gc_terreno t ON cu.ue_gc_terreno = t.id;
---
+++ etl/sql/queries/col_uebaunit_predio_unidadconstruccion.sql
@@ -0,0 +1,6 @@
+SELECT  
+gu.id as ue_gc_unidadconstruccion,
+unidad
+
+FROM {schema}.col_uebaunit cu
+JOIN {schema}.ue_gc_unidadconstruccion gu ON cu.ue_gc_unidadconstruccion = gu.id;
