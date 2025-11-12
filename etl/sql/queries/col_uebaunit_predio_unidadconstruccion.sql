23f8584ff422be0a4902fff47add6ba5db606ad4
1lcarlos <luiscardozoicg@gmail.com>
2025-11-12 18:14:42 -0500

    fix join unidad construccion

--- etl/sql/queries/col_uebaunit_predio_unidadconstruccion.sql
+++ etl/sql/queries/col_uebaunit_predio_unidadconstruccion.sql
@@ -3,4 +3,4 @@ gu.id as ue_gc_unidadconstruccion,
 unidad
 
 FROM {schema}.col_uebaunit cu
-JOIN {schema}.ue_gc_unidadconstruccion gu ON cu.ue_gc_unidadconstruccion = gu.id;
+JOIN {schema}.gc_unidadconstruccion gu ON cu.ue_gc_unidadconstruccion = gu.id;
